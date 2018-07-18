7929bf7b10be0b13762c17c161dede08396afd42
Thiago Alves <thiagofelicio@hotmail.con>
2018-07-18 18:57:52 -0300

    Initial commit

---
+++ app/src/main/java/alves/thiago/lanchonete/MainActivity.java
@@ -0,0 +1,27 @@
+package alves.thiago.lanchonete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    //Button button = findViewById(R.id.button);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+       // button.setOnClickListener(new View.OnClickListener() {
+      //      public void onClick(View v) {
+       //         Log.e("Teste","Teste");
+       //     }
+      //  });
+
+
+    }
+
+}
