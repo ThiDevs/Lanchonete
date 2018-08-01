70dc82122fd0c4fca6f731ea7f02808d02864e9c
Thiago <Thiagofelicio@hotmail.com>
2018-08-01 19:01:58 -0300

    50% complete

--- app/src/main/java/alves/thiago/lanchonete/Main2Activity.java
+++ app/src/main/java/alves/thiago/lanchonete/Main2Activity.java
@@ -9,13 +9,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.simplify.ink.InkView;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -26,6 +33,7 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,30 +45,37 @@ import static android.R.layout.simple_spinner_item;
 public class Main2Activity extends AppCompatActivity {
     AlertDialog.Builder alertDialogBuilder = null;
 
-    EditText Quantidade;
+    TextView Quantidade;
     EditText Colaborador;
     EditText Setor;
+    TextView Valor;
     Spinner s;
-    Spinner s2;
     List<String> item;
     List<String> item2;
     ArrayAdapter<String> adapter;
     ListView listview;
-    //ArrayAdapter<String> adapter2;
     List<String> value;
     String hostIP;
     ArrayAdapter<String> adapter2;
     Map<String, List<String>> MAP;
+    HashMap<String, Integer> items;
+    Intent intent;
+    InkView ink;
+    GestureDetector gestureDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        Button Finalizar;
+        Button Finalizar,Limpar_Dados;
         Finalizar = (Button) findViewById(R.id.Finalizar);
+        Limpar_Dados = (Button) findViewById(R.id.Limpar_Dados);
         Setor = (EditText) findViewById(R.id.Setor);
         Colaborador = (EditText) findViewById(R.id.Colaborador);
-        Quantidade = (EditText) findViewById(R.id.Quantidade);
+        Quantidade = (TextView) findViewById(R.id.Quantidade);
+        Valor = (TextView) findViewById(R.id.Valor);
+        ink = (InkView) findViewById(R.id.ink);
 
         MAP = MainActivity.getMAP();
         hostIP = MainActivity.getIP();
@@ -69,10 +84,14 @@ public class Main2Activity extends AppCompatActivity {
         listview  = (ListView) findViewById(R.id.list);
         value = new ArrayList<String>();
         value.add("");
-
+        items = new HashMap<String, Integer>();
+        intent = new Intent(this, MainActivity.class);
 
         adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, value);
         listview.setAdapter(adapter2);
+
+
+
 
 
 
@@ -88,11 +107,7 @@ public class Main2Activity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
-
        s = (Spinner) findViewById(R.id.spinner);
-
-
 
 
             Thread Get_Clientes = new Thread(new Runnable() {
@@ -163,24 +178,49 @@ public class Main2Activity extends AppCompatActivity {
 
         });
 
-       /* listview.setOnItemClickListener(
+        listview.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
 
-                        //String itemValue = (String) listview.getItemAtPosition(position);
-                        Thread EnviarIndexVideo = new Thread(new Runnable() {
-                            public void run() {
+                        String itemValue = (String) listview.getItemAtPosition(position);
+                        Log.d("MyApp",itemValue);
+
+                        String NomeEscolhido = itemValue.split(" [-] ")[0];
+                        System.out.println(NomeEscolhido);
+                        Float Valor_Separado = Float.valueOf(itemValue.split(" [-] ")[1]);
+                        Log.d("MyApp",String.valueOf(Valor_Separado));
+
+                        try{
+                            items.put(NomeEscolhido,items.get(NomeEscolhido)+1);
+                        } catch (Exception e){
+                            items.put(NomeEscolhido,1);
+                            e.printStackTrace();
+                        }
 
 
-                            }
 
-                        });
+                        if (Valor.getText().toString().equals("Valor")){
+                            Valor.setText("0");
+                        }
+                        if (Valor.getText() != null){
+                            String ValorSoma = Valor.getText().toString();
+                            Log.d("MyApp",ValorSoma);
+                            Float ValorSoma2 = Float.valueOf(ValorSoma);
+                            Float valortotal =  ValorSoma2 + Valor_Separado;
+                            Log.d("MyApp",String.valueOf(valortotal));
+                            Valor.setText(String.valueOf(valortotal));
+
+                            Quantidade.setText(items.toString().replaceAll("[{]","").replaceAll("[}]",""));
+
+
+
+                        }
+
 
 
                     }
                 });
-*/
 
 
         Finalizar.setOnClickListener(new View.OnClickListener() {
@@ -188,13 +228,14 @@ public class Main2Activity extends AppCompatActivity {
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(Main2Activity.this);
 
-                builder.setMessage("Você tem confirma que a/o "+ Colaborador.getText() +" do setor " + Setor.getText()
-                + " será cadastrado para o fiado do valor de R$ 5 reais?")
+                builder.setMessage("Você confirma que a/o "+ Colaborador.getText() +" do setor " + Setor.getText()
+                + " será cadastrado o pedido do valor de R$ "+Valor.getText()+" reais?")
                         .setTitle("Confirmação");
 
                 builder.setPositiveButton("Confirmo", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // User clicked OK button
+                        startActivity(intent);
+
                     }
                 });
                 builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -202,8 +243,6 @@ public class Main2Activity extends AppCompatActivity {
                         // User cancelled the dialog
                     }
                 });
-
-
 
                 AlertDialog dialog = builder.create();
                 dialog.show();
@@ -213,26 +252,51 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
+        Limpar_Dados.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Quantidade.setText("");
+                Setor.setText("");
+                Colaborador.setText("");
+                Valor.setText("Valor");
+                ink.clear();
+
+            }});
+
     }
 
     public void setItem(String Tipo){
 
         try {
             String[] stockArr2 = new String[MAP.get(Tipo).size()];
-            stockArr2 = MAP.keySet().toArray(stockArr2);
+            stockArr2 = MAP.get(Tipo).toArray(stockArr2);
 
+            Log.d("MyApp",MAP.toString());
 
+            value.clear();
 
             value.addAll(Arrays.asList(stockArr2));
         } catch (Exception e ){
             e.printStackTrace();
         }
 
+        try {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    adapter2.notifyDataSetChanged();
+                }
+            });
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
 
 
 
 
     }
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    };
 
 
 
--- app/src/main/java/alves/thiago/lanchonete/MainActivity.java
+++ app/src/main/java/alves/thiago/lanchonete/MainActivity.java
@@ -3,15 +3,10 @@ package alves.thiago.lanchonete;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -23,7 +18,6 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
